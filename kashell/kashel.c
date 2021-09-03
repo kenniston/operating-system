943057a8069746b495ce3ed04e4d70bfee4e02d2
Kenniston Arraes Bonfim <kenniston@gmail.com>
2021-09-03 02:36:46 -0300

    params parser

--- kashell/kashel.c
+++ kashell/kashel.c
@@ -6,17 +6,25 @@
 #include <sys/types.h>
 #include <ctype.h>
 
-#define MAX_CMD_SIZE 4096
+#define MAX_CMD_SIZE 512
+#define MAX_PARAMS 255
+#define MAX_OUT_FILE_PATH 255
+#define MAX_IN_FILE_PATH 255
+#define OUT_SUBCMD ">"
+#define IN_SUBCMD "<"
 
 typedef struct task {
     struct task *next;
     char cmd[MAX_CMD_SIZE];
+    char *params[MAX_PARAMS];
+    char outfile[MAX_OUT_FILE_PATH];
+    char infile[MAX_IN_FILE_PATH];
 } task_t;
 
 
 void str_trim(char * s) {
     char *start = s;
-    int length = strlen(start);
+    ulong length = strlen(start);
 
     while (isspace(start[length - 1])) {
         start[--length] = 0;
@@ -27,22 +35,48 @@ void str_trim(char * s) {
     memmove(s, start, length + 1);
 }
 
+void parse_cmd_params(task_t *task, char *params) {
+    char *paramstr = calloc(1, strlen(params) + 1);
+    strcpy(paramstr, params);
+
+    char *out = strstr(paramstr, OUT_SUBCMD);
+    if (out != NULL) {
+        strncpy(task->outfile, out + sizeof(OUT_SUBCMD), sizeof(task->outfile));
+        str_trim(task->outfile);
+    }
+
+    ulong length = out ? strlen(out) : 0;
+    char *cmd = calloc(1, strlen(paramstr) - length + 1);
+    strncpy(cmd, paramstr, strlen(paramstr) - length);
+    str_trim(cmd);
+
+    char *param;
+    int count = 0;
+    while ((param = strsep(&cmd, " "))) {
+        if (strcmp(task->cmd, "") == 0) {
+            strcpy(task->cmd, param);
+            continue;
+        }
+        task->params[count++] = param;
+    }
+}
+
 task_t* create_pipeline(char *str) {
     task_t *head = NULL;
     task_t *cur = NULL;
-    char *cmd;
-    char *cmdstr = malloc(strlen(str) + 1);
+    char *cmd = NULL;
+    char *cmdstr = calloc(1, strlen(str) + 1);
 
     strcpy(cmdstr, str);
     while ((cmd = strsep(&cmdstr, "|"))) {
-        task_t *t = malloc(sizeof(task_t));
+        task_t *t = calloc(1, sizeof(task_t));
         if (t != NULL) {
             head = head ?: t;
             if (cur != NULL) {
                 cur->next = t;
             }
             str_trim(cmd);
-            strcpy(t->cmd, cmd);
+            parse_cmd_params(t, cmd);
             cur = t;
         } else {
             //TODO: Error creating command. Stop pipeline.
@@ -58,7 +92,19 @@ void print_pipeline(task_t *tasks) {
     printf("Pipeline:\n");
     while (cur) {
         tmp = cur->next;
-        printf("\t=> %s\n", cur->cmd);
+        printf("\t=> %s", cur->cmd);
+        size_t n = sizeof cur->params / sizeof *cur->params;
+
+        char *p = NULL;
+        int count = 0;
+        while ((p = cur->params[count++]) != NULL) {
+            printf(" %s", p);
+        }
+
+        if (strcmp(cur->outfile, "") != 0) {
+            printf(" > %s", cur->outfile);
+        }
+        printf("\n");
         cur = tmp;
     }
 }
@@ -80,6 +126,7 @@ void read_cmd_loop() {
         char *cmd = 0;
         size_t cmdlen = 0;
         getline(&cmd, &cmdlen, stdin);
+        str_trim(cmd);
 
         if (strcasecmp(cmd, "quit") == 0) {
             break;
