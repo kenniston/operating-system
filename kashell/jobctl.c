c8ab61faed356041402a06ce5355e649fa25e056
Kenniston Arraes Bonfim <kenniston@gmail.com>
2021-09-19 20:52:14 -0300

    job control

--- kashell/jobctl.c
+++ kashell/jobctl.c
@@ -96,6 +96,10 @@ void run_single_process(task_t *task) {
     // Create a task child process.
     pid_t child_pid = fork();
     if (child_pid == 0) { // child process
+        // Create a new process group
+        pid_t pgid = getpid();
+        setpgid(pgid, pgid);
+
         // Configure input and output files.
         configure_stdinout(task);
 
@@ -105,6 +109,15 @@ void run_single_process(task_t *task) {
             perror("Error on command:");
         }
     } else { // kashell process
+        // Create a new process group.
+        // Setting process group on parent (shell)
+        // is to avoid race condition between parent
+        // and child process.
+        // https://www.gnu.org/software/libc/manual/html_node/Launching-Jobs.html
+        pid_t pgid = getpid();
+        setpgid(pgid, pgid);
+
+
         wait_child_process(child_pid);
     }
 }
@@ -126,6 +139,7 @@ void run_process_pipeline(task_t *tasks) {
     task_t *cur = tasks;
     int index = 0;
     int lastfd[2];
+    pid_t pgid = 0;
 
     // Creates the pipe file descriptors for interprocess communication.
     // The inoutfd[0] is the reader and inoutfd[1] is the writter.
@@ -139,6 +153,10 @@ void run_process_pipeline(task_t *tasks) {
         // Create a task child process.
         pid_t child_pid = fork();
         if (child_pid == 0) { // child process
+            // Create a new process group
+            if (pgid == 0) pgid = child_pid;
+            setpgid(child_pid, pgid);
+
             // Closes the write file descriptor in the child process
             // if this task is the last one. Otherwise, set the writer
             // file descriptor to the child process.
@@ -173,6 +191,14 @@ void run_process_pipeline(task_t *tasks) {
                 perror("Error on command:");
             }
         }
+        // Create a new process group.
+        // Setting process group on parent (shell)
+        // is to avoid race condition between parent
+        // and child process.
+        // https://www.gnu.org/software/libc/manual/html_node/Launching-Jobs.html
+        if (pgid == 0) pgid = child_pid;
+        setpgid(child_pid, pgid);
+
         // Closes the last file descriptors. If the descriptors
         // remain open, the child process will be blocked forever.
         close(lastfd[0]);
